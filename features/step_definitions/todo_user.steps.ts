cc1c5963234f46e97979816f92e039f871699c21
Mark Joseph R. Tigno <markj_tigno@hotmail.com>
2018-05-02 16:48:23 +0800

    Fixed some linting issues.

--- features/step_definitions/todo_user.steps.ts
+++ features/step_definitions/todo_user.steps.ts
@@ -12,24 +12,24 @@ export = function todoUserSteps() {
     this.setDefaultTimeout(30 * 1000);  // The todomvc.com website can sometimes be a bit slow to load, so we tell
                                         // Cucumber to give it up to 30 seconds to get ready.
 
-    let stage = serenity.callToStageFor({
-        actor: (name) => Actor.named(name).whoCan(BrowseTheWeb.using(protractor.browser))
+    const stage = serenity.callToStageFor({
+        actor: (name: string) => Actor.named(name).whoCan(BrowseTheWeb.using(protractor.browser))
     });
 
-    this.Given(/^.*that (.*) has a todo list containing (.*)$/, function (name: string, items: string) {
+    this.Given(/^.*that (.*) has a todo list containing (.*)$/, function(name: string, items: string) {
         return stage.theActorCalled(name).attemptsTo(
-            Start.withATodoListContaining(listOf(items))
+            Start.withATodoListContaining(listOf(items)),
         );
     });
 
-    this.When(/^s?he adds (.*?) to (?:his|her) list$/, function (itemName: string) {
+    this.When(/^s?he adds (.*?) to (?:his|her) list$/, function(itemName: string) {
         return stage.theActorInTheSpotlight().attemptsTo(
-            AddATodoItem.called(itemName)
-        )
+            AddATodoItem.called(itemName),
+        );
     });
 
-    this.Then(/^.* todo list should contain (.*?)$/, function (items: string) {
+    this.Then(/^.* todo list should contain (.*?)$/, function(items: string) {
         return expect(stage.theActorInTheSpotlight().toSee(TodoList.Items_Displayed))
-            .eventually.deep.equal(listOf(items))
+            .eventually.deep.equal(listOf(items));
     });
 };
